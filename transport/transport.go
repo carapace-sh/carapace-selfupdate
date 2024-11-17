4ade85608bc831848d72fc8dc6066aa3b798fafd
rsteube <rsteube@users.noreply.github.com>
2024-11-17 21:40:04 +0100

    tmp

--- transport/transport.go
+++ transport/transport.go
@@ -3,7 +3,7 @@ package transport
 import "io"
 
 type Transport interface {
-	Tags(repo string, out, outErr io.Writer) error
-	Assets(repo, tag string, out, outErr io.Writer) error
+	Tags(repo string, out, progress io.Writer) error
+	Assets(repo, tag string, out, progress io.Writer) error
 	Download(repo, tag, asset string, out, progress io.Writer) error
 }
