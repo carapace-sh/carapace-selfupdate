6e221c3fd6f73f647cab28b01f99aaedf8e08c67
rsteube <rsteube@users.noreply.github.com>
2024-11-17 15:36:24 +0100

    tmp

---
+++ checksum.go
@@ -0,0 +1,21 @@
+package selfupdate
+
+import (
+	"crypto/sha256"
+	"io"
+	"os"
+)
+
+func verify(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return err
+	}
+	return nil
+}
