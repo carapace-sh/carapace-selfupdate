4d41e99f177031ba87f4a992ce80a6ef76ecc105
rsteube <rsteube@users.noreply.github.com>
2025-02-09 12:21:29 +0100

    transport: fix broken pr

--- transport/curl.go
+++ transport/curl.go
@@ -8,24 +8,24 @@ import (
 
 type Curl struct{}
 
-func (c *Curl) retrieve(url string, out, progress io.Writer) error {
+func (c Curl) retrieve(url string, out, progress io.Writer) error {
 	command := exec.Command("curl", "-L", url)
 	command.Stdout = out
 	command.Stderr = progress
 	return command.Run()
 }
 
-func (c *Curl) Tags(repo string, out, progress io.Writer) error {
+func (c Curl) Tags(repo string, out, progress io.Writer) error {
 	url := fmt.Sprintf("https://api.github.com/repos/%v/tags", repo)
 	return c.retrieve(url, out, progress)
 }
 
-func (c *Curl) Assets(repo, tag string, out, progress io.Writer) error {
+func (c Curl) Assets(repo, tag string, out, progress io.Writer) error {
 	url := fmt.Sprintf("https://api.github.com/repos/%v/releases/tags/%v", repo, tag)
 	return c.retrieve(url, out, progress)
 }
 
-func (c *Curl) Download(repo, tag, asset string, out, progress io.Writer) error {
+func (c Curl) Download(repo, tag, asset string, out, progress io.Writer) error {
 	url := fmt.Sprintf("https://github.com/%v/releases/download/%v/%v", repo, tag, asset)
 	return c.retrieve(url, out, progress)
 }
--- transport/gh.go
+++ transport/gh.go
@@ -8,24 +8,24 @@ import (
 
 type Gh struct{}
 
-func (t *Gh) retrieve(args []string, out, progress io.Writer) error {
+func (t Gh) retrieve(args []string, out, progress io.Writer) error {
 	command := exec.Command("gh", args...)
 	command.Stdout = out
 	command.Stderr = progress
 	return command.Run()
 }
 
-func (t *Gh) Tags(repo string, out, progress io.Writer) error {
+func (t Gh) Tags(repo string, out, progress io.Writer) error {
 	args := []string{"api", fmt.Sprintf("https://api.github.com/repos/%v/tags", repo)}
 	return t.retrieve(args, out, progress)
 }
 
-func (t *Gh) Assets(repo, tag string, out, progress io.Writer) error {
+func (t Gh) Assets(repo, tag string, out, progress io.Writer) error {
 	args := []string{"api", fmt.Sprintf("repos/%v/releases/tags/%v", repo, tag)}
 	return t.retrieve(args, out, progress)
 }
 
-func (t *Gh) Download(repo, tag, asset string, out, progress io.Writer) error {
+func (t Gh) Download(repo, tag, asset string, out, progress io.Writer) error {
 	args := []string{"release", "download", "--repo", repo, tag, "--pattern", asset, "--output", "-"}
 	return t.retrieve(args, out, progress)
 }
